cdb125bbdd1273b36d3611997c049f909c177797
İclal Çetin <58071165+iclal07@users.noreply.github.com>
2024-10-20 21:26:58 +0300

    Update api.py

--- Chatbot/app/api.py
+++ Chatbot/app/api.py
@@ -1,1 +1,21 @@
+from fastapi import FastAPI
+from app.models import search
+from scripts.vectorize import load_documents, load_faiss_index
+from app.models import load_model
+
+app = FastAPI()
+
+# FAISS endeksini ve belgeleri yükle
+documents = load_documents()
+index = load_faiss_index()
+model = load_model()
+
+@app.get("/chatbot/")
+def get_response(query: str):
+    """
+    Sorguyu alır, en uygun cevabı döndürür.
+    """
+    results = search(query, model, index, documents)
+    return {"response": results[0]}
+
 
